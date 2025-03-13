7f8d29e0cbc6802d554912dc69fb888dbdd00f84
Björn Andersson <ba@sanitarium.se>
2025-03-13 21:40:13 +0800

    Rename input variable to name
    Because I don't want to use page because we're not dealing with only
    pages.

--- ppdefaults/partials.go
+++ ppdefaults/partials.go
@@ -14,9 +14,9 @@ type PartialsInFolderOnly struct {
 
 // Load gets files from a folder named after the passed in template and treats them as partials.
 // Ex: a template named "something/hello.tmpl" will load any files in the folder "something/hello/".
-func (p *PartialsInFolderOnly) Load(page string) ([]FileWithContent, error) {
-	ext := path.Ext(page)
-	dirName := strings.TrimSuffix(page, ext)
+func (p *PartialsInFolderOnly) Load(name string) ([]FileWithContent, error) {
+	ext := path.Ext(name)
+	dirName := strings.TrimSuffix(name, ext)
 
 	var files []FileWithContent
 	err := fs.WalkDir(p.FS, dirName, func(filePath string, entry fs.DirEntry, err error) error {
@@ -54,11 +54,11 @@ type PartialsWithCommon struct {
 }
 
 // Load partials in the same way as [PartialsInFolderOnly.Load] and from a CommonDir, for example "partials".
-func (p *PartialsWithCommon) Load(page string) ([]FileWithContent, error) {
+func (p *PartialsWithCommon) Load(name string) ([]FileWithContent, error) {
 	var files []FileWithContent
 
-	ext := path.Ext(page)
-	dirName := strings.TrimSuffix(page, ext)
+	ext := path.Ext(name)
+	dirName := strings.TrimSuffix(name, ext)
 
 	for _, dir := range []string{dirName, p.CommonDir} {
 		err := fs.WalkDir(p.FS, dir, func(filePath string, entry fs.DirEntry, err error) error {
